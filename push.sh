f9f4880a7d5b89af97fdbf182d8482d8f0b0f6d5
yyf <143@>
2017-05-05 16:43:28 +0800

    2017年 05月 05日 星期五 16:43:28 CST

--- push.sh
+++ push.sh
@@ -1,7 +1,10 @@
 CODES=~/Documents/codes/
+
+cd $CODES/php/sites/s02
 cp $CODES/database/setup.sql files/
 cp $CODES/database/sql01.sql files/
 cp $CODES/database/mysql01.sh files/
+
 
 git add --all
 git commit -am "$(date)"
